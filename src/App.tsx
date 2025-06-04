4864c249a35923e9770e91beb4bb0073d6a837a4
Evandro Jun Ishikawa <evandro.ishikawa@sympla.com.br>
2025-06-04 15:40:30 -0300

    Project structure

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <Header />
+
+      <main></main>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
